08bce6917741a6e54d8610f13027ffe8d2ce35a5
Anthony Lewis <me@anthonylewis.com>
2012-12-05 19:18:50 -0600

    Session Five Changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,14 @@
 class ProjectsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    debugger
+    logger.debug "*** Starting index action ***"
+    logger.debug "Page = #{params[:page]}"
+    @projects = @projects.includes(:tasks).page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,8 +19,6 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
-    @project = Project.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @project }
@@ -24,8 +28,6 @@ class ProjectsController < ApplicationController
   # GET /projects/new
   # GET /projects/new.json
   def new
-    @project = Project.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @project }
@@ -34,14 +36,11 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
-    @project = Project.find(params[:id])
   end
 
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(params[:project])
-
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
@@ -56,8 +55,6 @@ class ProjectsController < ApplicationController
   # PUT /projects/1
   # PUT /projects/1.json
   def update
-    @project = Project.find(params[:id])
-
     respond_to do |format|
       if @project.update_attributes(params[:project])
         format.html { redirect_to @project, notice: 'Project was successfully updated.' }
@@ -72,7 +69,6 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     respond_to do |format|
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,15 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
-  attr_accessible :private, :title
+  attr_accessible :private, :title, :user_id
   validates :title, :presence => true
   validates :user, :presence => true, :if => :private
+
+  def user_name
+    if user
+      user.name
+    else
+      "unassigned"
+    end
+  end
 end
